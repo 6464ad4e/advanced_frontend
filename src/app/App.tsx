fb09c06cf1827d6d5210fdea6c10b867e593f59b
6464ad4e <107412723+6464ad4e@users.noreply.github.com>
2025-05-01 13:59:12 +0500

    init: restore a project

---
+++ src/app/App.tsx
@@ -0,0 +1,26 @@
+import { Suspense } from 'react';
+import { AppRouter } from 'app/providers/router';
+import { Navbar } from 'widgets/Navbar';
+import { Sidebar } from 'widgets/Sidebar';
+import { useTheme } from 'app/providers/ThemeProvider';
+import { classNames } from 'shared/lib/classNames/classNames';
+
+import './styles/index.scss';
+
+function App() {
+  const { theme } = useTheme();
+
+  return (
+    <div className={classNames('app', {}, [theme])}>
+      <Suspense fallback=''>
+        <Navbar />
+        <div className='content-page'>
+          <Sidebar />
+          <AppRouter />
+        </div>
+      </Suspense>
+    </div>
+  );
+}
+
+export default App;
